8bd34c4452f908cd078cc45f2df9ab6d15bdf984
Godwin Homadzi <godwinhomadzi820@gmail.com>
2025-02-21 07:39:18 -0800

    finglargestnumber

---
+++ findthelargestnumber.js
@@ -0,0 +1,21 @@
+function findLargest(a,b,c){
+    if(typeof a !=='number' ||typeof b !=='number' || typeof c !== 'number'){
+        return"Invalid Input" ;
+    }
+
+
+if (a >= b && a >= c ){
+    return a;
+}
+else if (b >= a && b >= c) {
+    return b;
+}
+else {
+    return c;
+}
+}
+
+
+console.log(findLargest(5,10,3))
+console.log(findLargest(20,20,5))
+console.log(findLargest("x",10,3))
